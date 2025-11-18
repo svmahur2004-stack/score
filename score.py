c8a96f004f17eed926e1d4597fd5842fd33747ca
svmahur2004-stack <svmahur2004@gmail.com>
2025-11-18 20:15:53 +0530

    saved

---
+++ score.py
@@ -0,0 +1,20 @@
+import sys
+
+if len(sys.argv) > 1:
+    script_name = sys.argv[0]
+    scores = [float(score) for score in sys.argv[1:]]
+    print("User provided input values:")
+    print("Script name:", script_name)
+    print("Scores:", scores)
+    print("Sum:", sum(scores))
+    print("Average:", sum(scores) / len(scores))
+    
+else:
+    script_name = sys.argv[0]
+    scores = [90, 80, 70, 60, 50]
+    print("No input given:")
+    print("Script name:", script_name)
+    print("Scores:", scores)
+    print("Sum:", sum(scores))
+    print("Average:", sum(scores) / len(scores))
+   
